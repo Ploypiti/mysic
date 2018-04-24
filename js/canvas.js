5383e13e9621a46c0caf302f222db5f56bfac207
Ploypiti Piyaprapan <ploypitisweet@gmail.com>
2018-04-24 15:53:53 +0700

    Add canvas (50% done)
    Frontend only

---
+++ js/canvas.js
@@ -0,0 +1,23 @@
+/*expand toolbar*/
+function expand(e) {
+var allExpanded = document.querySelectorAll( "div[id^='expanded_']" );
+Array.from( allExpanded ).forEach( s => (s.style.display = "none") );
+document.getElementById("expanded_" + e).style.display = "block";
+document.getElementById("toolbar_expand").style.display = "block";
+}
+
+/*sliders*/
+/*volume*/
+var volume = document.getElementById("volume_s");
+var v_output = document.getElementById("volume_value");
+v_output.innerHTML = volume.value + "px";
+volume.oninput = function() {
+  v_output.innerHTML = this.value + "px";
+}
+/*trim*/
+var trimtime = document.getElementById("trim_s");
+var t_output = document.getElementById("trim_value");
+t_output.innerHTML = volume.value + "s";
+trimtime.oninput = function() {
+  t_output.innerHTML = this.value + "s";
+}
