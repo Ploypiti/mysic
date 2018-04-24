470d685ef0ccf8900028dc53ae2e5635e23e7de7
Nisaruj <nisaruj@hotmail.com>
2018-04-24 18:42:56 +0700

    Add auth system

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var router = express.Router()
+var auth = require('../controllers/auth')
+
+//Get homepage
+router.get('/', function(req, res){
+    res.render('index', {user: req.user});
+})
+
+//Get canvas
+router.get('/canvas', function(req, res){
+    res.render('canvas')
+})
+
+//Get login
+router.get('/signin', auth.get_login)
+
+//Get register
+router.get('/signup', function(req, res){
+    res.render('register')
+})
+
+router.post('/signin', auth.post_login)
+
+router.post('/signup', auth.post_register)
+
+router.get('/signout', auth.get_logout)
+
+module.exports = router;
