5ca0ff24e4af9250c1d1ee083d0a7fe58b445e1c
Ploypiti Piyaprapan <ploypitisweet@gmail.com>
2018-04-29 17:54:05 +0700

    Play-Pause

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,4 +1,4 @@
-/*expand toolbar*/
+//expand toolbar
 function expand(e) {
 var allExpanded = document.querySelectorAll( "div[id^='expanded_']" );
 Array.from( allExpanded ).forEach( s => (s.style.display = "none") );
@@ -6,18 +6,18 @@ document.getElementById("expanded_" + e).style.display = "block";
 document.getElementById("toolbar_expand").style.display = "block";
 }
 
-/*sliders*/
-/*volume*/
+//sliders
+//volume
 var volume = document.getElementById("volume_s");
 var v_output = document.getElementById("volume_value");
 v_output.innerHTML = volume.value + "px";
 volume.oninput = function() {
   v_output.innerHTML = this.value + "px";
 }
-/*trim*/
+//trim
 var trimtime = document.getElementById("trim_s");
 var t_output = document.getElementById("trim_value");
-t_output.innerHTML = volume.value + "s";
+t_output.innerHTML = trimtime.value + "s";
 trimtime.oninput = function() {
   t_output.innerHTML = this.value + "s";
 }
